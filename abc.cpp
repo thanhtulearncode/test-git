ae272f64b4ca30f0a351a0b4bfe38673f01aaac3
thanhtulearncode <thanh_tu.vo@insa-cvl.fr>
2024-10-22 01:08:33 +0700

    Add files via upload

---
+++ abc.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	iso_base:sync_with_stdio(false);
+	cin.tie(nullptr);
+	//freopen("file.INP", "r", stdin);
+	//freopen("file.OUT", "w", stdout);
+	//string s;
+	//getline(cin, s);
+    int n;
+    cin>>n;
+    while(n--)
+
+}
